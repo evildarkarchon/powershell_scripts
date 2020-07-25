9047d7d143966d601cbb7ec7b8ef53c0216e2be8
Andrew Nelson <evildarkarchon@gmail.com>
2020-07-25 15:21:48 -0700

    Using a try-finally block to hopefully make the script always go to the previous directory when exiting for any reason.
    Also, if intermediate directory does not exist, create it.

--- Download-Twitch-VoD.ps1
+++ Download-Twitch-VoD.ps1
@@ -9,25 +9,32 @@ param (
     [string[]]$Urls
 )
 $PreviousDirectory = Get-Location
-Set-Location $IntermediateDir
+try {
+    if (-not (Test-Path -PathType Any $IntermediateDir)) {
+        New-Item -ItemType Directory -Path $IntermediateDir -Force
+    }
+    Set-Location $IntermediateDir
 
-if (-not (Test-Path -PathType Any "Z:\Videos\Twitch\$($Streamer)")) { 
-    New-Item -ItemType Directory -Path "Z:\Videos\Twitch\$($Streamer)" -Force
-    New-Item -ItemType File -Path "Z:\Videos\Twitch\$($Streamer)\downloaded.txt"
-    (Get-Item -path "Z:\Videos\Twitch\$($Streamer)\downloaded.txt").Attributes += "Hidden"
-    New-Item -ItemType File -Path "Z:\Videos\Twitch\$($Streamer)\downloaded_low.txt"
-    (Get-Item -path "Z:\Videos\Twitch\$($Streamer)\downloaded_low.txt").Attributes += "Hidden"
-}
+    if (-not (Test-Path -PathType Any "Z:\Videos\Twitch\$($Streamer)")) { 
+        New-Item -ItemType Directory -Path "Z:\Videos\Twitch\$($Streamer)" -Force
+        New-Item -ItemType File -Path "Z:\Videos\Twitch\$($Streamer)\downloaded.txt"
+        (Get-Item -path "Z:\Videos\Twitch\$($Streamer)\downloaded.txt").Attributes += "Hidden"
+        New-Item -ItemType File -Path "Z:\Videos\Twitch\$($Streamer)\downloaded_low.txt"
+        (Get-Item -path "Z:\Videos\Twitch\$($Streamer)\downloaded_low.txt").Attributes += "Hidden"
+    }
 
-if ($Force) {
-    youtube-dl --config-location "Z:\Videos\$($Quality)_force.conf" $Urls
-}
-else {
-    youtube-dl --config-location "Z:\Videos\$($Quality).conf" --download-archive "Z:\Videos\Twitch\$($Streamer)\downloaded.txt" $Urls
-}
+    if ($Force) {
+        youtube-dl --config-location "Z:\Videos\$($Quality)_force.conf" $Urls
+    }
+    else {
+        youtube-dl --config-location "Z:\Videos\$($Quality).conf" --download-archive "Z:\Videos\Twitch\$($Streamer)\downloaded.txt" $Urls
+    }
 
-foreach ($file in Get-ChildItem $IntermediateDir) {
-    Write-Host "Moving '$($IntermediateDir)\$($file.Name)' to 'Z:\Videos\Twitch\$($Streamer)\$($file.Name)'"
-    Move-item -Path $file.Name -Destination "z:\Videos\Twitch\$($Streamer)\$($file.Name)"
+    foreach ($file in Get-ChildItem $IntermediateDir) {
+        Write-Host "Moving '$($IntermediateDir)\$($file.Name)' to 'Z:\Videos\Twitch\$($Streamer)\$($file.Name)'"
+        Move-item -Path $file.Name -Destination "z:\Videos\Twitch\$($Streamer)\$($file.Name)"
+    }
 }
-Set-Location $PreviousDirectory
+finally {
+    Set-Location $PreviousDirectory  
+}
