eb7565d0e3d30eb43b90078be81596e812bb7d77
Andrew Nelson <evildarkarchon@gmail.com>
2020-08-06 22:15:38 -0700

    Genericize Download-Twitch-VoD and replace Download-Video with it.

--- Download-Video.ps1
+++ Download-Video.ps1
@@ -1,135 +1,53 @@
 using namespace System.Collections.Generic
 [CmdletBinding(DefaultParameterSetName="Download")]
 param (
-    [Parameter(ParameterSetName="Batch")]
     [Parameter(ParameterSetName="Download")]
-    [string]$ConfigFile="z:\videos\720p.conf",
     [Parameter(ParameterSetName="Batch")]
+    [string]$Quality="480p",
     [Parameter(ParameterSetName="Download")]
+    [Parameter(ParameterSetName="Batch")]
+    [string]$IntermediateDir,
+    [Parameter(ParameterSetName="Download")]
+    [Parameter(ParameterSetName="Batch")]
+    [string]$OutDir,
+    [Alias("ConfigFile")]
+    [Parameter(ParameterSetName="Download")]
+    [Parameter(ParameterSetName="Batch")]
+    [string]$ConfigDir="Z:\Videos",
+    [Parameter(ParameterSetName="Download")]
+    [Parameter(ParameterSetName="Batch")]
     [switch]$Force,
     [Parameter(ParameterSetName="Test")]
     [switch]$ListFormats,
-    [Parameter(ParameterSetName="Batch")]
+    [Alias("Streamer")]
     [Parameter(ParameterSetName="Download")]
-    [string]$OutDir,
     [Parameter(ParameterSetName="Batch")]
-    [Parameter(ParameterSetName="Download")]
-    [string]$IntermediateDir,
-    [Parameter(ParameterSetName="Batch")]
-    [Parameter(ParameterSetName="Test")]
-    [string]$BatchFile,
-    [Parameter(Position=0, ValueFromRemainingArguments=$true, ParameterSetName="Download")]
-    [Parameter(Position=0, ValueFromRemainingArguments=$true, ParameterSetName="Test")]
-    [string[]]$Urls
+    [string]$Producer,
+    [Parameter(Mandatory=$true, Position=0, ValueFromRemainingArguments=$true, ParameterSetName="Download")]
+    [Parameter(Mandatory=$false, Position=0, ValueFromRemainingArguments=$true, ParameterSetName="Test")]
+    [string[]]$Urls,
+    [Parameter(Mandatory=$true, ParameterSetName="Batch")]
+    [Parameter(Mandatory=$false, ParameterSetName="Test")]
+    [string]$BatchFile
 )
-$PreviousLocation = Get-Location
+$PreviousDirectory = Get-Location
 function YoutubeDL {
     param (
         [Parameter(Mandatory=$true)]
-        [string[]]$Options
+        [string[]]$Options,
+        [Parameter(Mandatory=$true)]
+        $Output
     )
+    Set-Location $Output
     youtube-dl $Options
 }
-
-function New-Foundation {
-    if (-not (Test-Path -PathType Any $OutDir) -and -not [string]::IsNullOrEmpty($OutDir) -and -not $ListFormats) {
-        New-Item -ItemType Directory -Path $OutDir -Force
-        if (-not $Force) {
-            New-Item -ItemType File -Path "$($OutDir)\downloaded.txt"
-            (Get-Item -LiteralPath "$($OutDir)\downloaded.txt").Attributes += "Hidden"
-            New-Item -ItemType File -Path "$($OutDir)\downloaded_low.txt"
-            (Get-Item -LiteralPath "$($OutDir)\downloaded_low.txt").Attributes += "Hidden"
-        }
-    }
-}
-
 try {
     $YtDlOptions = [List[string]]::new()
-    if (-not [string]::IsNullOrEmpty($IntermediateDir) -and -not $ListFormats) {
-        if (-not (Test-Path -PathType Any $IntermediateDir)) {
-            New-Item -ItemType Directory -Path $IntermediateDir -Force
-        }
-
-        New-Foundation
-
-        Set-Location $IntermediateDir
-        
-        if (-not (Test-Path -PathType Any "$($IntermediateDir)\downloaded_ps.txt") -and -not $Force){
-            New-Item -ItemType File -Path "$($IntermediateDir)\downloaded_ps.txt"
-            (Get-Item -LiteralPath "$($IntermediateDir)\downloaded_ps.txt").Attributes += "Hidden"
-        }
-        
-        if ($Force) {
-            if (-not [string]::IsNullOrEmpty($BatchFile)) {
-                foreach ($i in @("--config-location", $ConfigFile, "-a", $BatchFile)) {
-                    $YtDlOptions.Add($i)
-                }
-            }
-            else {
-                foreach ($i in @("--config-location", $ConfigFile)) {
-                    $YtDlOptions.Add($i)
-                }
-                $YtDlOptions.AddRange($Urls)
-            }
-        }
-        else {
-            if (-not [string]::IsNullOrEmpty($OutDir)) {
-                if (-not [string]::IsNullOrEmpty($BatchFile)) {
-                    foreach ($i in @("--config-location", $ConfigFile, "--download-archive", "$($OutDir)\downloaded.txt", "-a", $BatchFile)) {
-                        $YtDlOptions.Add($i)
-                    }
-                }
-                else {
-                    foreach ($i in @("--config-location", $ConfigFile, "--download-archive", "$($OutDir)\downloaded.txt")) {
-                        $YtDlOptions.Add($i)
-                    }
-                    $YtDlOptions.AddRange($Urls)
-                }
-            }
-            else {
-                if (-not [string]::IsNullOrEmpty($BatchFile)) {
-                    foreach ($i in @("--config-location", $ConfigFile, "--download-archive", "$($IntermediateDir)\downloaded_ps.txt", "-a", $BatchFile)) {
-                        $YtDlOptions.Add($i)
-                    }
-                }
-                else {
-                    foreach ($i in @("--config-location", $ConfigFile, "--download-archive", "$($IntermediateDir)\downloaded_ps.txt")) {
-                        $YtDlOptions.Add($i)
-                    }
-                    $YtDlOptions.AddRange($Urls)
-                }
-            }
-        }
-    }
-    elseif (-not [string]::IsNullOrEmpty($OutDir) -and -not $ListFormats){
-        #if (-not (Test-Path -PathType Any $OutDir)) {
-        #    New-Item -ItemType Directory -Path $OutDir -Force
-        #    if (-not $Force) {
-        #        New-Item -ItemType File -Path "$($OutDir)\downloaded.txt"
-        #        (Get-Item -path "$($OutDir)\downloaded.txt").Attributes += "Hidden"
-        #        New-Item -ItemType File -Path "$($OutDir)\downloaded_low.txt"
-        #        (Get-Item -path "$($OutDir)\downloaded_low.txt").Attributes += "Hidden"
-        #    } 
-        #}
-        
-        New-Foundation
-        
-        Set-Location $OutDir
-        if (-not [string]::IsNullOrEmpty($BatchFile)) {
-            foreach ($i in @("--config-location", $ConfigFile, "-a", $BatchFile)) {
-                $YtDlOptions.Add($i)
-            }
-        }
-        else {
-            foreach ($i in @("--config-location", $ConfigFile)) {
-                $YtDlOptions.Add($i)
-            }
-            $YtDlOptions.AddRange($Urls)
-        }
-    }
-    elseif ($ListFormats) {
+    [string]$Destination
+    
+    if ($ListFormats -eq $true) {
         $YtDlOptions.Add("--list-formats")
-        if (-not [string]::IsNullOrEmpty($BatchFile)) {
+        if (-not [string]::IsNullOrEmpty($BatchFile)){
             foreach ($i in @("-a", $BatchFile)) {
                 $YtDlOptions.Add($i)
             }
@@ -137,42 +55,87 @@ try {
         else {
             $YtDlOptions.AddRange($Urls)
         }
+        YoutubeDL $YtDlOptions.ToArray() "none"
     }
     else {
-        if (-not [string]::IsNullOrEmpty($BatchFile)) {
-            foreach ($i in @("--config-location", $ConfigFile, "-a", $BatchFile)) {
+        if (-not [string]::IsNullOrEmpty($IntermediateDir) -and -not (Test-Path -PathType Any $IntermediateDir)) {
+            New-Item -ItemType Directory -Path $IntermediateDir -Force
+        }
+        if ([string]::IsNullOrEmpty($Producer) -and -not [string]::IsNullOrEmpty($OutDir)){
+            $Destination = $OutDir
+        }
+        elseif (-not [string]::IsNullOrEmpty($Producer) -and -not [string]::IsNullOrEmpty($OutDir)) {
+            $Destination = "$($OutDir)\$($Producer)"
+        }
+        elseif (-not [string]::IsNullOrEmpty($Producer) -and [string]::IsNullOrEmpty($OutDir)) {
+            $Destination = "$(Get-Location)\$($Producer)"
+        }
+        else {
+            $Destination = (Get-Location)
+        }
+        if (-not (Test-Path -PathType Any $Destination) -and $CreateDirectories -eq $true) { 
+            New-Item -ItemType Directory -Path $Destination -Force
+            if (-not $Force) {
+                New-Item -ItemType File -Path "$($Destination)\downloaded.txt"
+                (Get-Item -path "$($Destination)\downloaded.txt").Attributes += "Hidden"
+                New-Item -ItemType File -Path "$($Destination)\downloaded_low.txt"
+                (Get-Item -path "$($Destination)\downloaded_low.txt").Attributes += "Hidden"
+            }
+        }
+
+        if ((Test-Path $ConfigDir -PathType Leaf)) {
+            foreach ($i in @("--config-location", $ConfigDir)) {
+                $YtDlOptions.Add($i)
+            }
+        }
+        elseif ($Force) {
+            foreach ($i in @("--config-location", "$($ConfigDir)\$($Quality)_force.conf")) {
                 $YtDlOptions.Add($i)
             }
         }
         else {
-            foreach ($i in @("--config-location", $ConfigFile)) {
+            foreach ($i in @("--config-location", "$($ConfigDir)\$($Quality).conf")) {
                 $YtDlOptions.Add($i)
             }
-            $YtDlOptions.AddRange($Urls)
         }
-    }
-    
-    YoutubeDL($YtDlOptions.ToArray())
-
-    if (-not [string]::IsNullOrEmpty($IntermediateDir) -and [string]::IsNullOrEmpty($OutDir) -and -not $ListFormats) {
-        foreach ($file in Get-ChildItem $IntermediateDir -Exclude "*.txt","*.ytdl","*.part") {
-            Write-Host "[powershell] Moving '$($IntermediateDir)\$($file.Name)' -> '$($PreviousLocation)\$($file.Name)'"
-            Move-Item -LiteralPath "$($IntermediateDir)\$($file.Name)" -Destination "$($PreviousLocation)\$($file.Name)"
-            if (Test-Path $file -PathType Any) {
-                Remove-Item -Force $file
+        if ([string]::IsNullOrEmpty($Producer)) {
+            if (-not [string]::IsNullOrEmpty($BatchFile)) {
+                foreach ($i in @("--download-archive", "$($Destination)\downloaded.txt", "-a", $BatchFile)) {
+                    $YtDlOptions.Add($i)
+                }
+            }
+            else {
+                foreach ($i in @("--download-archive", "$($Destination)\downloaded.txt")) {
+                    $YtDlOptions.Add($i)
+                }
+                $YtDlOptions.AddRange($Urls)
+            }
+        }
+        else {
+            if (-not [string]::IsNullOrEmpty($BatchFile)) {
+                foreach ($i in @("--download-archive", "$($Destination)\downloaded.txt", "-a", $BatchFile)) {
+                    $YtDlOptions.Add($i)
+                }
+            }
+            else {
+                foreach ($i in @("--download-archive", "$($Destination))\downloaded.txt")) {
+                    $YtDlOptions.Add($i)
+                }
+                $YtDlOptions.AddRange($Urls)
             }
         }
     }
-    elseif (-not [string]::IsNullOrEmpty($IntermediateDir) -and -not [string]::IsNullOrEmpty($OutDir) -and -not $ListFormats) {
-        foreach ($file in Get-ChildItem $IntermediateDir -Exclude "*.txt","*.ytdl","*.part") {
-            Write-Host "[powershell] Moving '$($IntermediateDir)\$($file.Name)' -> '$($OutDir)\$($file.Name)"
-            Move-Item -LiteralPath "$($IntermediateDir)\$($file.Name)" -Destination "$($OutDir)\$($file.Name)"
-            if (Test-Path $file -PathType Any) {
-                Remove-Item -Force $file
+        YoutubeDL $YtDlOptions.ToArray() $Destination
+        if (-not [string]::IsNullOrEmpty($IntermediateDir)) {
+            foreach ($file in Get-ChildItem $IntermediateDir -Exclude "*.txt","*.ytdl","*.part") {
+                Write-Host "[powershell] Moving '$($IntermediateDir)\$($file.Name)' -> '$($Destination)\$($file.Name)'"
+                Move-Item -LiteralPath "$($IntermediateDir)\$($file.Name)" -Destination "$($Destination)\$($file.Name)"
+                if (Test-Path $file -PathType Any) {
+                    Remove-Item -Force $file
+                }
             }
         }
-    }
 }
 finally {
-    Set-Location $PreviousLocation
+    Set-Location $PreviousDirectory
 }
