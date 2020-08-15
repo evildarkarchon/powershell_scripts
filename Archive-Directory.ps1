82bcdbd1733be1c6cd03b97b871e01c0c8feaca0
Andrew Nelson <evildarkarchon@gmail.com>
2020-08-15 02:16:57 -0700

    Add & to and replacement and formatting change

--- Archive-Directory.ps1
+++ Archive-Directory.ps1
@@ -9,7 +9,8 @@ param (
 foreach ($i in get-childitem $Directory) {
     $tablenameSB = [StringBuilder]::new()
     [void]$tablenameSB.Append($i.Name)
-    [void]$tablenameSB.Replace(".", "_").Replace(" ", "_").Replace("'", "_").Replace(",", "").Replace("/", "_").Replace("\", "_").Replace("-", "_").Replace("#", "")
+    [void]$tablenameSB.Replace(".", "_").Replace(" ", "_").Replace("'", "_").Replace(",", "").Replace("/", "_").Replace("\", "_").Replace("-", "_").Replace("#", "").Replace("&", "and")
     $tablename = $tablenameSB.ToString().ToLower()
-    sqlite_archive $DatabaseLocation add -t $tablename "$($i.FullName)" 
+    #write-host $i.FullName
+    sqlite_archive $DatabaseLocation add -t $tablename $i.FullName 
 }
