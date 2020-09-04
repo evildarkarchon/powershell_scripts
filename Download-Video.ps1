f2b5d9a4abac3d7a6c5a899cdb5681485efa7174
Andrew Nelson <evildarkarchon@gmail.com>
2020-09-03 22:57:17 -0700

    Remove most commented out code, move archive file options foreach statement to execute just after file creation.

--- Download-Video.ps1
+++ Download-Video.ps1
@@ -1,5 +1,5 @@
 using namespace System.Collections.Generic
-using namespace System.Text
+#using namespace System.Text
 [CmdletBinding(DefaultParameterSetName="Download")]
 param (
     [Parameter(ParameterSetName="Download")]
@@ -53,7 +53,6 @@ else {
     $Quality = $Quality -replace '[^0-9]', ''
 }
 
-#write-host $Quality
 function YoutubeDL {
     param (
         [Parameter(Mandatory=$true)]
@@ -86,35 +85,12 @@ try {
             New-Item -ItemType Directory -Path $IntermediateDir -Force
         }
         
-        #$DestinationSB = [StringBuilder]::new()
-        #if (-not [string]::IsNullOrEmpty($OutDir)){
-        #    [void]$DestinationSB.Append($OutDir)
-        #    if (-not [string]::IsNullOrEmpty($Producer)) {
-        #        [void]$DestinationSB.Append("\")
-        #        [void]$DestinationSB.Append($Producer)
-        #    }
-        #}
-        #else {
-        #    [void]$DestinationSB.Append($PreviousDirectory)
-        #    if (-not [string]::IsNullOrEmpty($Producer) -and [string]::IsNullOrEmpty($OutDir)) {
-        #        [void]$DestinationSB.Append("\")
-        #        [void]$DestinationSB.Append($Producer)
-        #    }
-        #}
-
-        #if (-not [string]::IsNullOrEmpty($Series)) {
-        #    [void]$DestinationSB.Append("\")
-        #    [void]$DestinationSB.Append($Series)
-        #}
-
-        # $Destination = $DestinationSB.ToString()
         if ([string]::IsNullOrEmpty($OutDir)) {
             $Destination = (Join-Path $PreviousDirectory $Producer $Series)
         }
         else {
             $Destination = (Join-Path $OutDir $Producer $Series)
         }
-        # write-host $Destination
 
         if (-not (Test-Path -PathType Any $Destination)) { 
             New-Item -ItemType Directory -Path $Destination -Force
@@ -131,6 +107,10 @@ try {
                 New-Item -ItemType File -Path $ArchiveFile
                 (Get-Item -path $ArchiveFile).Attributes += "Hidden"
                 }
+            
+            foreach ($i in @("--download-archive", $ArchiveFile)) {
+                $YtDlOptions.Add($i)
+            }
         }
 
         if (-not [string]::IsNullOrEmpty($Config) -and (Test-Path $Config -PathType Leaf)) {
@@ -138,23 +118,23 @@ try {
                 $YtDlOptions.Add($i)
             }
         }
+        
         if ($Quality.ToLower() -eq "source") {
             $FormatString = '(bestvideo+bestaudio/best)[ext = webm]/bestvideo+bestaudio/best'
         }
         else {
             $FormatString = "(bestvideo+bestaudio/best)[height <=? $($Quality)][fps <=? $($FrameRate)][ext = webm]/(bestvideo+bestaudio/best)[height <=? $($Quality)][fps <=? $($FrameRate)]/(bestvideo+bestaudio/best)[fps <=? $($FrameRate)]/bestvideo+bestaudio/best"
         }
+        
         foreach ($i in @("-f", $FormatString)) {
             $YtDlOptions.Add($i)
         }
 
         if ($AutoNumber) {
             if (-not [string]::IsNullOrEmpty($IntermediateDir)) {
-                # $WhereTo = @("-o", "$($IntermediateDir)\%(autonumber)s - %(title)s-%(id)s_%(height)sp@%(fps)s.%(ext)s")
                 $WhereTo = @("-o", (Join-Path $IntermediateDir "%(autonumber)s - %(title)s-%(id)s_%(height)sp@%(fps)s.%(ext)s"))
             }
             else {
-                # $WhereTo = @("-o", "$($Destination)\%(autonumber)s - %(title)s-%(id)s_%(height)sp@%(fps)s.%(ext)s")
                 $WhereTo = @("-o", (Join-Path $Destination "%(autonumber)s - %(title)s-%(id)s_%(height)sp@%(fps)s.%(ext)s"))
             }
             foreach ($i in $WhereTo) {
@@ -163,25 +143,12 @@ try {
         }
         else {
             if (-not [string]::IsNullOrEmpty($IntermediateDir)) {
-                # $WhereTo = @("-o", "$($IntermediateDir)\%(title)s-%(id)s_%(height)sp@%(fps)s.%(ext)s")
                 $WhereTo = @("-o", (Join-Path $IntermediateDir "%(title)s-%(id)s_%(height)sp@%(fps)s.%(ext)s"))
             }
             else {
-                # $WhereTo = @("-o", "$($Destination)\%(title)s-%(id)s_%(height)sp@%(fps)s.%(ext)s")
                 $Whereto = @("-o", (Join-Path $Destination "%(title)s-%(id)s_%(height)sp@%(fps)s.%(ext)s"))
             }
             foreach ($i in $WhereTo) {
-                $YtDlOptions.Add($i)
-            }
-        }
-        if (-not $Force) {
-            if ([int]$Quality -lt 480) {
-                $Archive = "downloaded_low.txt"
-            }
-            else {
-                $Archive = "downloaded.txt"
-            }
-            foreach ($i in @("--download-archive", (Join-Path $Destination $Archive))) {
                 $YtDlOptions.Add($i)
             }
         }
@@ -201,7 +168,6 @@ try {
     }
 
     if (-not $ListFormats) {
-        # write-host $YtDlOptions.ToArray()
         YoutubeDL $YtDlOptions.ToArray()
     }
 }
